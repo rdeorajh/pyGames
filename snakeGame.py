84b746b55437d26935283bbb164e7dba5f744d95
rdeorajh <ryandeorajh@gmail.com>
2021-04-08 13:26:05 -0400

    Added snake eating feature. Basic barebones game working.

--- snakeGame.py
+++ snakeGame.py
@@ -13,7 +13,7 @@ class windowObject(object):
         self.win = pygame.display.set_mode((screenWidth,screenHeight))
         pygame.display.set_caption("Snaaaake")  
     def drawWindow(self):
-        self.win.fill((0,0,0))
+        self.win.fill((255,255,255))
 
 def collision(snakeWinObj,snakeObj):
     collide = False
@@ -22,22 +22,31 @@ def collision(snakeWinObj,snakeObj):
     #Collision with itself
     for i in range(1, snakeObj.tailSize):
         if xHead == snakeObj.X[i] and yHead == snakeObj.Y[i]:
-            print("Collision!!!!!!")
+            #print("Collision!!!!!!")
             collide = True
     return collide
 
 def eatFoodCheck(foodObj,snakeObj):
-    pass
+    eatFood = False
+    xHead = snakeObj.X[0]
+    yHead = snakeObj.Y[0]
+    
+    if xHead == foodObj.xFood and yHead == foodObj.yFood:
+        #print("Food!")
+        eatFood = True
+    
+    return eatFood
 
 
 #Game Variables
 crash = False
+score = 0
 scrWidth = 800
 scrHeight = 600
-delayTime = 150
+delayStep = 10
 blockSize = 20
 snakeLenInc = 5
-foodHitbox = True
+foodHitbox = False
 
 
 snakeWin = windowObject(scrWidth,scrHeight)
@@ -46,8 +55,9 @@ while run:
     clock.tick(60)
 
     #On Startup
+    delayTime = 150
     snake1 = snake(scrWidth/2,scrHeight/2,blockSize)
-    snake1.updateSnakeSize(20)
+    snake1.updateSnakeSize(snakeLenInc)
     snake1.left = True #Get rid of this later
     food = snakeFood(blockSize,snakeWin)
     food.newFoodPos(snake1,snakeWin)
@@ -66,15 +76,25 @@ while run:
         keys = pygame.key.get_pressed()
         snake1.updateHeadPos(keys,snakeWin)
 
+        crash = collision(snakeWin,snake1)
+
+        if eatFoodCheck(food,snake1):
+            score += 10
+            #print(score)
+            snake1.updateSnakeSize(snakeLenInc)
+            food.newFoodPos(snake1,snakeWin)
+            if delayTime > 50:
+                delayTime -= delayStep
+            elif delayTime > 0:
+                delayTime -= delayStep//2
+            print(delayTime) 
+
         #Draw
         snakeWin.drawWindow()
         snake1.draw(snakeWin)
         food.drawFood(snakeWin,foodHitbox)
         pygame.display.update()
         
-
-        crash = collision(snakeWin,snake1)
-
         #DelayTimer
         pygame.time.delay(delayTime)
 
